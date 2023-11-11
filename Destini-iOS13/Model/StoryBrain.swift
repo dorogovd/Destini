604840aa7e81218336cfd4c86aaa50cdcb3f8819
Dima <dorogovd@yandex.ru>
2023-11-11 16:03:36 +0300

    Create a Structure and add files

--- Destini-iOS13/Model/StoryBrain.swift
+++ Destini-iOS13/Model/StoryBrain.swift
@@ -8,4 +8,10 @@
 
 import Foundation
 
-
+struct StoryBrain {
+    let story = [
+        Story(storyText: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
+        Story(storyText: "You see a tiger", choice1: "Shout for help", choice2: "Play dead"),
+        Story(storyText: "You find a treasure chest", choice1: "Open it", choice2: "Check for traps")
+    ]
+}
